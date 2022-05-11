91ff7a27c080c1bde0a776c3af7cb0e54f8e3569
ospreystudio <64425019+ospreystudio@users.noreply.github.com>
2022-05-11 14:51:29 +0400

    todo list

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Abut Vitrify Todo</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet aut cumque deserunt doloribus, earum error est eum illo in inventore molestias necessitatibus non optio praesentium sapiente suscipit, veniam voluptate.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dicta dolorum fugiat impedit, incidunt inventore ipsa ipsum maiores modi nesciunt nihil non nostrum odio odit officia, provident qui temporibus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet aut cumque deserunt doloribus, earum error est eum illo in inventore molestias necessitatibus non optio praesentium sapiente suscipit, veniam voluptate.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto commodi dicta dolorum fugiat impedit, incidunt inventore ipsa ipsum maiores modi nesciunt nihil non nostrum odio odit officia, provident qui temporibus.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,98 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <div class="home pa-6">
+    <v-text-field
+        v-model="newTaskTitle"
+        @click:append="addTask"
+        @keyup.enter="addTask"
+        class="pa-3"
+        outlined
+        label="Add Task"
+        append-icon="mdi-plus"
+        hide-details
+        clearable
+    ></v-text-field>
+    <v-list class="pt-0"
+        flat
+    >
+
+        <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="doneTask(task.id)"
+        :class="{'blue lighten-5' : task.done}">
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through' : task.done }"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+
+          </template>
+
+
+        </v-list-item>
+          <v-divider></v-divider>
+        </div>
+
+
+    </v-list>
+  </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+  return {
+    newTaskTitle: "",
+    tasks: [
+      // {
+      //   id: 1,
+      //   title: "Wake up",
+      //   done: false
+      // },
+      // {
+      //   id: 2,
+      //   title: "Get bananas",
+      //   done: true
+      // },
+      // {
+      //   id: 3,
+      //   title: "Eat Bananas",
+      //   done: false
+      // },
+    ]
+  }
+},
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    },
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ""
+
+    }
   }
 }
 </script>
